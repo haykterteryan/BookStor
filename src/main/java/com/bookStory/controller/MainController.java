0396b5384168ee8757cfea92aa8419906aabfcb4
Barev Baylus <alo@nsn.cc>
2019-07-05 20:29:28 +0400

    Controllers and checkink with cURL  is done

--- src/main/java/com/bookStory/controller/MainController.java
+++ src/main/java/com/bookStory/controller/MainController.java
@@ -1,14 +1,46 @@
 package com.bookStory.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.bookStory.domain.Book;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
 
 @RestController
+@RequestMapping("/")
 public class MainController {
 
-    @RequestMapping("/")
-    public String name(){
-        return "Hello world";
+    @RequestMapping(method = RequestMethod.GET)
+    public ResponseEntity getByAuthor(@RequestParam("author") String author){
+        System.out.println(author);
+
+        return ResponseEntity.ok().build();
     }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public ResponseEntity  add(@RequestBody Book book){
+        System.out.println(book);
+
+        return ResponseEntity.ok().build();
+    }
+
+    @RequestMapping(value = "{id}", method = RequestMethod.PUT)
+    public ResponseEntity  update(@PathVariable("id") int id, @RequestBody Book book){
+        System.out.println(id + " " + book);
+
+        return ResponseEntity.ok().build();
+    }
+
+    @RequestMapping(value = "{id}", method = RequestMethod.DELETE)
+    public ResponseEntity  delete(@PathVariable int id){
+        System.out.println(id);
+
+        return ResponseEntity.ok().build();
+    }
+
+
+
+
+
+
 
 }
